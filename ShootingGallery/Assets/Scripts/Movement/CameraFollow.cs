b277ae00d9ea8cc3ca6fa6462e1261f69f8c9183
matiaspierpaoli <matiaspierpaoli@gmail.com>
2023-07-01 16:06:31 -0300

    Fixed camera position to avoid unpleasent collisions

--- ShootingGallery/Assets/Scripts/Movement/CameraFollow.cs
+++ ShootingGallery/Assets/Scripts/Movement/CameraFollow.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour 
+public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform playerCameraPosition;
 
     private void LateUpdate()
     {
-        transform.position = playerCameraPosition.position;
+        transform.position = playerCameraPosition.position + playerCameraPosition.TransformDirection(Vector3.zero);
     }
 }
